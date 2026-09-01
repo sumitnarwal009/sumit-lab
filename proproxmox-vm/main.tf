bc424246a360da7a8cabeb0b8684a5b0c37af4f3
sumitnarwal009 <148902605+sumitnarwal009@users.noreply.github.com>
2026-09-01 10:02:34 -0500

    Add Proxmox VM configuration in main.tf

--- proproxmox-vm/main.tf
+++ proproxmox-vm/main.tf
@@ -1,4 +1,52 @@
-proproxmox-vm/
-    main.tf
-    variables.tf
-    terraform.tfvars
+terraform {
+  required_providers {
+    proxmox = {
+      source  = "bpg/proxmox"
+      version = ">= 0.60.0, < 1.0.0"
+    }
+  }
+}
+
+provider "proxmox" {
+  endpoint  = var.proxmox_api_url
+  api_token = var.proxmox_api_token
+  insecure  = var.proxmox_tls_insecure
+}
+
+resource "proxmox_virtual_environment_vm" "this" {
+  name      = var.vm_name
+  node_name = var.proxmox_node
+  vm_id     = var.vm_id
+
+  clone {
+    vm_id = var.template_vm_id
+    full  = true
+  }
+
+  cpu {
+    cores = var.vm_cores
+  }
+
+  memory {
+    dedicated = var.vm_memory
+  }
+
+  disk {
+    datastore_id = var.disk_datastore
+    interface    = "scsi0"
+    size         = var.disk_size
+  }
+
+  network_device {
+    bridge = var.network_bridge
+  }
+
+  initialization {
+    ip_config {
+      ipv4 {
+        address = var.vm_ip_address
+        gateway = var.vm_gateway
+      }
+    }
+  }
+}
